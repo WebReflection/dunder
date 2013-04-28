99d06fa66700678faf39433c054a5f1aefbe3d0a
Andrea Giammarchi <agiammarchi@twitter.com>
2013-04-27 23:03:12 -0700

    made the test a bit more meaningful

--- test/dunder.js
+++ test/dunder.js
@@ -1,13 +1,15 @@
 //remove:
-var main = require('../build/dunder.node.js');
+var dunder = require('../build/dunder.node.js');
 //:remove
 
 wru.test([
   {
     name: "main",
     test: function () {
-      wru.assert(typeof main == "function");
-      // wru.assert(0);
+      wru.assert(typeof dunder == "function");
+      var a = {}, b = {};
+      a = dunder(a, b);
+      wru.assert(b.isPrototypeOf(a));
     }
   }
 ]);
